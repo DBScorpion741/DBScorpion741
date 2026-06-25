a347c1622a6cafc7d1b0b5e9d4e7d9ebc9557aec
DBScorpion741 <162214019+DBScorpion741@users.noreply.github.com>
2026-06-24 19:38:47 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll('.links a');
+
+links.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        links.forEach(l => l.classList.remove('active'));
+        
+        this.classList.add('active');
+    });
+});
