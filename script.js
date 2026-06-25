70419e38a746746aada79110c4d7aa86d7ae5ed6
DBScorpion741 <162214019+DBScorpion741@users.noreply.github.com>
2026-06-24 20:28:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const navIcons = document.querySelectorAll('.nav-icon');
 
 navIcons.forEach(icon => {
     icon.addEventListener('click', function() {
+        // Quitamos la clase 'active' de todos los elementos
         navIcons.forEach(i => i.classList.remove('active'));
+        // Añadimos 'active' solo al elemento clicado
         this.classList.add('active');
     });
 });
